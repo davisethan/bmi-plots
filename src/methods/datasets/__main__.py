9c7cde3b21cd3249778d0c62e9cf4c8ef4216381
Ethan Davis <89031823+davisethan@users.noreply.github.com>
2026-04-08 14:14:17 -0700

    Created validation scripts in bmi (#11)

--- src/methods/datasets/__main__.py
+++ src/methods/datasets/__main__.py
@@ -1,12 +1,6 @@
-from .montage import Montage
-from .bubble_plot import BubblePlot
 from .psd import PSD
 from .edf import EDF
-from .channels import Channels
 
 if __name__ == "__main__":
-    Montage().run()
-    BubblePlot().run()
     PSD().run()
     EDF().run()
-    Channels().run()
